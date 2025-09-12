e5c9e01d92a2f35494d0adfbd0d80b745277c9c3
Devendra Singh <devthedeveloperguy@gmail.com>
2025-09-12 15:50:35 +0530

    feat: add new project 'firstbook'

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,16 @@
 export const projects = [
 	{
 		id: 1,
+		title: "firstbook",
+		description:
+			"An open, lightweight take on NotebookLM — focused on clarity, sources, and real-time context.",
+		stack: ["nextjs", "typescript", "tailwind", "postgres", "better-auth", "ai-sdk", "exa ai"],
+		link: "https://github.com/raodevendrasingh/firstbook",
+		show: true,
+		year: "2025",
+	},
+	{
+		id: 2,
 		title: "voltx",
 		description: "A CLI tool to supercharge your terminal with Native AI.",
 		stack: ["typescript", "nodejs"],
@@ -9,7 +19,7 @@ export const projects = [
 		year: "2025",
 	},
 	{
-		id: 2,
+		id: 3,
 		title: "gradhunt",
 		description: "Job Listing and candidate management portal.",
 		stack: ["react", "typescript", "python", "django", "tailwind", "postgres", "clerk auth"],
@@ -18,7 +28,7 @@ export const projects = [
 		year: "2024",
 	},
 	{
-		id: 3,
+		id: 4,
 		title: "testimo",
 		description: "Collect testimonials and export them as ready-to-use react components.",
 		stack: ["nextjs", "typescript", "tailwind", "mongodb", "next-auth"],
